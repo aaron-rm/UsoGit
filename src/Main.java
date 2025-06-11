afc0f254e4d9e8a352be77a0dd8d1a86a7cd722b
Aaron <aaron.remarchuk@utp.ac.pa>
2025-06-10 20:38:18 -0500

    Bases del programa
    variables a usar

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+//Un operador bancario está creando una lista diaria de n clientes calificados para
+//préstamos por valor entre B/ 25000.00 (25mil) y B/ 400000.00 (400mil). Se toman los
+//datos personales: nombre, sexo, salario y el monto de su préstamo. Si su salario es
+//mayor a B/.800.00, el cliente califica para el préstamo. Indique al cliente si se le aprobó
+//o no el préstamo.
+//El operador dejará de prestar hasta que cumpla o sobrepase su meta de 1 millón de
+//balboas por día (1000000.00)
+//El programa debe mostrar la cantidad de clientes tramitados, a cuantos clientes se les
+//aprobó préstamo. Imprima cuantos hombres y cuantas mujeres que aprobaron su
+//préstamo.
+//Trabaje colaborativamente usando Github. Tome en cuenta todas las excepciones y
+//validaciones necesarias.
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
     public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
 
+        }catch (RuntimeException e){
+            System.err.println("Error: " + e.getMessage());
+        }
     }
 }
