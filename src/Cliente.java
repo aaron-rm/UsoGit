cd1d966075ddcc69904fec000e08dc6be4fc0241
Aaron <aaron.remarchuk@utp.ac.pa>
2025-06-12 08:30:41 -0500

    datos
    pedi unos cuantos datos, faltan los calculos y mostrar la informacion

--- src/Cliente.java
+++ src/Cliente.java
@@ -2,4 +2,16 @@ public class Cliente {
     private String nombre,sexo;
     private double salario,prestamo;
 
+//Se toman los
+////datos personales: nombre, sexo, salario y el monto de su préstamo. Si su salario es
+////mayor a B/.800.00, el cliente califica para el préstamo. Indique al cliente si se le aprobó
+////o no el préstamo.
+////El operador dejará de prestar hasta que cumpla o sobrepase su meta de 1 millón de
+////balboas por día (1000000.00)
+    public Cliente(String nombre, String sexo, double salario){
+        this.nombre=nombre;
+        this.sexo=sexo;
+
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
+
 //Un operador bancario está creando una lista diaria de n clientes calificados para
-//préstamos por valor entre B/ 25000.00 (25mil) y B/ 400000.00 (400mil). Se toman los
-//datos personales: nombre, sexo, salario y el monto de su préstamo. Si su salario es
-//mayor a B/.800.00, el cliente califica para el préstamo. Indique al cliente si se le aprobó
-//o no el préstamo.
-//El operador dejará de prestar hasta que cumpla o sobrepase su meta de 1 millón de
-//balboas por día (1000000.00)
-//El programa debe mostrar la cantidad de clientes tramitados, a cuantos clientes se les
-//aprobó préstamo. Imprima cuantos hombres y cuantas mujeres que aprobaron su
-//préstamo.
-//Trabaje colaborativamente usando Github. Tome en cuenta todas las excepciones y
-//validaciones necesarias.
+//préstamos por valor entre B/ 25000.00 (25mil) y B/ 400000.00 (400mil).
+//Se toman los
+////datos personales: nombre, sexo, salario y el monto de su préstamo. Si su salario es
+////mayor a B/.800.00, el cliente califica para el préstamo. Indique al cliente si se le aprobó
+////o no el préstamo.
+////El operador dejará de prestar hasta que cumpla o sobrepase su meta de 1 millón de
+////balboas por día (1000000.00)
+//El programa debe mostrar la cantidad de clientes tramitados, a cuantos clientes se les aprobó préstamo.
+// Imprima cuantos hombres y cuantas mujeres que aprobaron su préstamo.
+
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -18,11 +18,42 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        try {
-            System.out.println("Hola");
+        int cantClientes = 0;
+        while (true){
+            try {
+                System.out.println("Inserte la cantidad de clientes: ");
+                cantClientes = Integer.parseInt(reader.readLine());
 
-        }catch (RuntimeException e){
-            System.err.println("Error: " + e.getMessage());
+                System.out.println("Datos del Cliente");
+                System.out.println("Nombre: ");
+                String nombre = reader.readLine().trim();
+
+                System.out.printf("Sexo: ");
+                String sexo = reader.readLine().trim();
+
+                System.out.println("Salario: ");
+                double salario = Double.parseDouble(reader.readLine());
+
+                System.out.println("Monto del préstamo: ");
+
+
+
+
+
+            }
+            catch (NumberFormatException e){
+                System.err.println("Error: " + e.getMessage());
+                continue;
+            }
+            catch (RuntimeException e){
+                System.err.println("Error: " + e.getMessage());
+                continue;
+            }
+            catch (IOException e){
+                System.err.println("Error: " + e.getMessage());
+                continue;
+            }
+            break;
         }
     }
 }
